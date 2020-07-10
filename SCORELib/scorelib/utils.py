483a8e2d449e71bf5d39d6ba076ab7cfe590fc53
Spl3en <spl3en.contact@gmail.com>
2020-07-10 12:06:03 +0200

    Add get_enum_name

--- SCORELib/scorelib/utils.py
+++ SCORELib/scorelib/utils.py
@@ -17,8 +17,13 @@
 from iconservice import *
 from .consts import *
 
+
 class Utils():
 
     @staticmethod
     def enum_names(cls):
         return [i for i in cls.__dict__.keys() if i[:1] != '_']
+
+    @staticmethod
+    def get_enum_name(cls, index):
+        return Utils.enum_names(cls)[index]
